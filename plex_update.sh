988008dea90b9eb15779699b1e86d0675ad19f01
alvaromm6556 <74918315+alvaromm6556@users.noreply.github.com>
2022-02-08 13:05:44 +0100

    Plex Updater

---
+++ plex_update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Quick shell script to automatically download and install
+# the latest version of Plex Media Server on Debian or Ubuntu.
+#
+# The filters look for Debian/Ubuntu strings only
+# but can easily be adapted to Fedora or MacOS if you're a pervert
+# or hipster respectively.
+#
+# requirements: apt-get install curl jq
+
+# Insane JQ parsing to select both build AND distro.
+plex_url=$(curl -s https://plex.tv/pms/downloads/5.json \
+ | jq -r '.computer.Linux.releases[] | select((.build=="linux-x86_64") and .distro=="debian") .url')
+
+# Download the latest version of plex and install it
+curl -O $plex_url && dpkg -i ${plex_url##*/} 
+
+sudo rm plex*.deb
+sudo systemctl start plexmediaserver.service
+sudo chmod -R 775 /tank/Peliculas/*
+sudo chown -R administrador:administrador /tank/Peliculas/*
