86607f7e1fe5dafd89d79d69cbaa0f699a69f544
alvaromm6556 <74918315+alvaromm6556@users.noreply.github.com>
2022-08-04 12:53:50 +0200

    Delete installdockerinlxc.sh

--- Scripts/installdockerinlxc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update && apt upgrade -y # Actualiza el sistema
-apt install docker.io -y # Instala Docker
-systemctl start docker # Inicia el Servicio de Docker
-systemctl enable docker # Inicia los contenedores de Docker al iniciar el LXC
