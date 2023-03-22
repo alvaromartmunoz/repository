1dc0591462ad239d37e4e3ff276efbe046844b94
alvaromm6556 <74918315+alvaromm6556@users.noreply.github.com>
2023-03-22 08:26:17 +0100

    Update initial_centos.sh

--- Scripts/initial_centos.sh
+++ Scripts/initial_centos.sh
@@ -1,4 +1,9 @@
-sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
-sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
-dnf clean all
-dnf swap centos-linux-repos centos-stream-repos
+if [[ $EUID -ne 0 ]]; then
+   	echo "This script must be run as root" 
+   	exit 1
+else
+    sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+    sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+    dnf clean all
+    dnf swap centos-linux-repos centos-stream-repos
+fi
