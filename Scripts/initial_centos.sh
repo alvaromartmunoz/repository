ab4d386863236e3f9cb14c8b2aa07d19bd7f0548
alvaromm6556 <74918315+alvaromm6556@users.noreply.github.com>
2023-03-22 08:25:37 +0100

    Create initial_centos.sh

---
+++ Scripts/initial_centos.sh
@@ -0,0 +1,4 @@
+sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
+dnf clean all
+dnf swap centos-linux-repos centos-stream-repos
